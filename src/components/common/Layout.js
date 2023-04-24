6035b5a2ad7dc2c649c23882a3f9a886149846a6
Jason <jason@jcode.me>
2023-04-24 10:26:06 +0930

    Remove comments and stats

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import { Commento, Footer } from '.'
+import { Footer } from '.'
 import { cloudinary } from '../../utils/helper'
 
 // Styles
@@ -22,29 +22,15 @@ const DefaultLayout = ({
     children,
     bodyClass,
     isHome,
-    showComments,
     slug,
 }) => {
     const site = data.allGhostSettings.edges[0].node
-    const [showLoadCommentsButton, setLoadComments] = React.useState(true)
 
-    const loadComments = () => {
-        setLoadComments(false)
-        try{
-            umami('Loading comments');
-        } catch (e) {}
-        window.commento.main()
-    }
     return (
         <>
             <Helmet>
                 <html lang={site.lang} />
                 <style type="text/css">{`${site.codeinjection_styles}`}</style>
-                <script async defer
-                    data-website-id="9bc8c034-3fce-4653-b88e-ea795bf82e13"
-                    src={`/js/u.js`}
-                    data-host-url="https://stats.jcode.me"
-                ></script>
                 <body className={bodyClass} />
             </Helmet>
 
@@ -101,16 +87,6 @@ const DefaultLayout = ({
                                                 d="M512 144v288c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48h88l12.3-32.9c7-18.7 24.9-31.1 44.9-31.1h125.5c20 0 37.9 12.4 44.9 31.1L376 96h88c26.5 0 48 21.5 48 48zM376 288c0-66.2-53.8-120-120-120s-120 53.8-120 120 53.8 120 120 120 120-53.8 120-120zm-32 0c0 48.5-39.5 88-88 88s-88-39.5-88-88 39.5-88 88-88 88 39.5 88 88z"></path>
                                         </svg>
                                     </a>
-                                    <a className="social-link social-link-tw" href="https://twitter.com/majorlawnlids" title="Twitter">
-                                        <div className={`sr-only`}>Twitter</div>
-
-                                        <svg aria-hidden="true" data-prefix="fab" data-icon="twitter"
-                                            className="svg-inline--fa fa-twitter fa-w-16" role="img"
-                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                            <path fill="currentColor"
-                                                d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
-                                        </svg>
-                                    </a>
                                 </div>
                             </nav>
                         </div>
@@ -122,21 +98,6 @@ const DefaultLayout = ({
                             {children}
                         </div>
                     </main>
-
-                    { showComments ?
-                        <div className={`comment-section`}>
-                            <div className="inner">
-                                { showLoadCommentsButton ?
-                                    <div className="show-more-button">
-                                        <a className="button large" href="#" onClick={loadComments}>
-                                            Load comments
-                                        </a>
-                                    </div>
-                                    : null }
-                                <Commento id={ slug } />
-                            </div>
-                        </div> :
-                        null }
                 </div>
 
                 <div className="viewport-bottom">
@@ -156,7 +117,6 @@ DefaultLayout.propTypes = {
         file: PropTypes.object,
         allGhostSettings: PropTypes.object.isRequired,
     }).isRequired,
-    showComments: PropTypes.bool,
     slug: PropTypes.string,
 }
 
